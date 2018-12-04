2520c9b2f0137b92010a03b8bb664439e5da1be8
Chris Joyce <ctjoyce@gmail.com>
2018-12-03 20:03:45 -0700

    Uses redis to track where we are in the scraping process.

--- scraper/src/scraper.ts
+++ scraper/src/scraper.ts
@@ -17,15 +17,26 @@
 
 import * as puppeteer from 'puppeteer';
 import * as _ from 'lodash';
+import * as redis from 'redis';
+
+const { promisify } = require('util');
+const client = redis.createClient();
+const getAsync = promisify(client.get).bind(client);
+
 import { RequestParser, ConsoleLogger } from './utils';
+
 // import fs from 'fs';
 // import logger from 'logger';
 // declare var location: any;
 
+client.on('error', (err) => {
+  console.log('Error with redis: ' + err);
+});
 // const MIN_PRICE: number = 60;
 // const ITEM_CONDITION: {} = {};
 
 
+const unprocessedItems: string = 'unprocessedItems';
 const SOLD_ROOT: string =
   'https://www.ebay.com/b/Mens-Athletic-Shoes/15709/bn_57918?LH_ItemCondition=1500&LH_Sold=1&rt=nc&_dmd=1&_sop=13&_udlo=75&_pgn=1';
 //example of an "view original link"
@@ -43,7 +54,7 @@ const pageIdentifier: string = '&_pgn='
 const priceIdentifier: string = '&_udlo=';
 
 
-// function nextPage(page: puppeteer.Page): void {}
+function nextPage(page: puppeteer.Page): void { }
 
 function handlePaginationReturn(element: Element) {
   // console.log(element.innerHTML);
@@ -55,16 +66,30 @@ function handlePaginationReturn(element: Element) {
   // await page.click()
 }
 
-async function gotoNextPage(currentUrl: string, currentPage: number): Promise<string> {
-  currentPage++;
+function gotoNextPage(currentUrl: string, currentPage: number): string {
   const parsed_url: URL = new URL(currentUrl);
   parsed_url.searchParams.set('_pgn', `${currentPage}`);
   return parsed_url.href;
 }
 
+async function fetchLastItemFound(): Promise<string> {
+  return await getAsync('lastProcessedItemId');
+}
+
+async function DEBUG_override(): Promise<void> {
+  await client.set('lastProcessedItemId', '163409080976', redis.print);
+}
+
 async function StartSoldSearch(): Promise<any> {
-  let foundLastItem: boolean = false;
+  let foundLastItemIndex: number = -1;
   let currentPage: number = 1;
+
+  // await DEBUG_override();
+  let lastProcessedItemId: string = await fetchLastItemFound();
+  let currentEndId: string;
+
+  console.log(`Last processed item retrieved: ${lastProcessedItemId}`);
+
 
   browser = await puppeteer.launch();
   page = await browser.newPage();
@@ -72,66 +97,65 @@ async function StartSoldSearch(): Promise<any> {
   page.on('console', ConsoleLogger);
   page.on('request', RequestParser);
 
-  let currentUrl: string = SOLD_ROOT;
-  let itemsToProcess: any[] = [];
-
-  // while (!foundLastItem) {
-  console.log('Parsing page: ', currentPage);
+  page.setUserAgent('Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.102 Safari/537.36');
   await page.setRequestInterception(true);
   await page.setJavaScriptEnabled(false);
-  page.setUserAgent('Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.102 Safari/537.36');
-  await page.goto(currentUrl, { waitUntil: 'networkidle2' });
+
+  let currentUrl: string = SOLD_ROOT;
+  let itemsToProcess: any[] = [];
+  let finishedParsing: boolean = false;
+
+  while (!finishedParsing && currentPage < 20) {
+
+    console.log('Parsing page: ', currentPage);
+    console.log(`url: ${currentUrl}`);
+    await page.goto(currentUrl, { waitUntil: 'networkidle2' });
 
 
-  let sold_shoes: any[] = await page.$$eval('.s-item', (links) => {
-    return links.map(link => {
-      const el = link.querySelector('.s-item__image > a');
-      const url: URL = new URL(el.getAttribute('href'));
-      const id: string = url.pathname.slice(url.pathname.lastIndexOf('/') + 1);
-      return {
-        id,
-        outer: link.outerHTML
-      }
-    })
-  });
+    let sold_shoes: any[] = await page.$$eval('.s-item', (links) => {
+      return links.map(link => {
+        const el = link.querySelector('.s-item__image > a');
+        const url: URL = new URL(el.getAttribute('href'));
+        const id: string = url.pathname.slice(url.pathname.lastIndexOf('/') + 1);
+        return {
+          id,
+          outer: link.outerHTML
+        }
+      })
+    });
 
-  // // sold_shoes.forEach(shoe => { console.log(new URL(shoe.url)) });
-  // sold_shoes.forEach(s => console.log(s.id));
-
-  // function parseIDs(shoes: string[]): string[] {
-  //   const blah: {}[] = _.map(shoes, shoe => {
-  //     return {};
-  //   });
-  //   return blah;
-  // }
-  // sold_shoes = parseIDs(sold_shoes);
-
-  itemsToProcess = [...itemsToProcess, ...sold_shoes];
-
-  currentUrl = await gotoNextPage(currentUrl, currentPage)
-  console.log(`${sold_shoes.length} items added to the processor`);
-  console.log(`${itemsToProcess.length} left to process`);
-  // console.log(sold_shoes);
-  //    {
-  //     return {
-  //       children: link.children[0],
-  //       id: link.querySelector('s-item__item-id.s-item__itemID'),
-  //       // outer: link.outerHTML,
-  //       // inner: link.innerHTML,
-  //       // text: link.textContent
-  //     }
-  //     // return link.outerHTML;
-  //   });
-  // });
-
-  // console.log(sold_shoes);
+    foundLastItemIndex = sold_shoes.findIndex(shoe => {
+      console.log(`${shoe.id} - ${lastProcessedItemId}`);
+      return shoe.id === lastProcessedItemId
+    });
 
 
+    console.log(`Last Item: ${lastProcessedItemId} was found: ${foundLastItemIndex}`);
+
+    if (foundLastItemIndex !== -1) {
+      finishedParsing = true;
+      sold_shoes.splice(foundLastItemIndex);
+    }
+
+    if (currentPage === 1 && sold_shoes.length > 0) {
+      currentEndId = sold_shoes[0].id;
+      client.set('lastProcessedItemId', currentEndId, redis.print);
+    }
 
 
-  //   gotoNextPage();
-  //   currentPage++;
-  // };
+    sold_shoes.forEach(item => {
+      client.rpush(unprocessedItems, JSON.stringify(item), redis.print);
+    });
+
+
+    client.llen('unprocessedItems', (err: any, numberOfItems: number) => {
+      console.log(`Items left to process: ${numberOfItems}`);
+    });
+
+    console.log(`${sold_shoes.length} items added to the processor`);
+
+    currentUrl = gotoNextPage(currentUrl, ++currentPage)
+  };
 
 
   // parseShoes(sold_shoes);
@@ -139,6 +163,7 @@ async function StartSoldSearch(): Promise<any> {
 
 
   await browser.close();
+  client.quit();
 }
 
 
